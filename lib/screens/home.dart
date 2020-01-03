4bfdb23f370ec9e243115ca4f9f4a6f4be33f281
Ian Murray <ianmurrays@gmail.com>
2020-01-03 16:51:55 +0100

    Simple UI proof of concept

---
+++ lib/screens/home.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Oh Tee Pee'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () => print('settings'),
+          ),
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () => print('edit'),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => print('add'),
+        splashColor: Colors.blueGrey,
+      ),
+      body: ListView(
+        children: <Widget>[
+          _randomTile(),
+          _randomTile(),
+          _randomTile(),
+          _randomTile(),
+          _randomTile(),
+          _randomTile(),
+        ],
+      ),
+    );
+  }
+
+  Widget _randomTile() {
+    return ListTile(
+      leading: Container(
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          color: Colors.blueGrey,
+          shape: BoxShape.circle
+        ),
+        child: Center(
+          child: Text(
+            'G',
+            style: TextStyle(fontSize: 25, color: Colors.white)
+          )
+        ),
+      ),
+      title: Text('Google'),
+      subtitle: Text('email@gmail.com'),
+      trailing: TOTPNumber(),
+    );
+  }
+}
+
+class TOTPNumber extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TOTPNumberState();
+  }
+}
+
+class TOTPNumberState extends State<TOTPNumber> {
+  int _value = 0;
+  String _calculatedValue = '000000';
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(Duration(seconds: 5), _timerCallback);
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this._calculatedValue, style: TextStyle(fontSize: 35));
+  }
+
+  void _timerCallback(Timer timer) {
+    print('timer!!');
+    setState(() {
+      _value = Random().nextInt(1000000);
+
+      _calculatedValue = _value.toString().padLeft(6, '0');
+    });
+  }
+}
