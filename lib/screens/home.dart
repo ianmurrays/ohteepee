51a652417c7da62c71d9843eea2700566b87c17f
Ian Murray <ianmurrays@gmail.com>
2021-03-06 13:13:01 +0100

    Remove unused import

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../widgets/passwords_list_view.dart';
 import '../widgets/home_floating_action_button.dart';
