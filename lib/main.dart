c718318a36d55f6b3c75cb713f6de36593199728
Ian Murray <ianmurrays@gmail.com>
2021-03-06 13:01:54 +0100

    Enable higher refresh rates

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 
 import './providers/global_timer.dart';
 import './providers/passwords.dart';
 import './screens/home.dart';
 
-void main() {
+void main() async {
+  await _unlockRefreshRate();
+
   runApp(OhTeePee());
+}
+
+// From https://github.com/bnxm/shared/blob/master/shared/lib/utils/android_refreshrate_unlocker.dart
+Future<void> _unlockRefreshRate() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    final current = await FlutterDisplayMode.current;
+    final modes = await FlutterDisplayMode.supported;
+
+    DisplayMode bestMode = current;
+
+    for (final mode in modes) {
+      final isFaster = mode.refreshRate > bestMode.refreshRate;
+      final isSameResolution =
+          mode.width == bestMode.width && mode.height == bestMode.height;
+
+      if (isFaster && isSameResolution) {
+        bestMode = mode;
+      }
+    }
+
+    FlutterDisplayMode.setMode(bestMode);
+  } catch (e) {
+    print(e);
+  }
 }
 
 class OhTeePee extends StatelessWidget {
